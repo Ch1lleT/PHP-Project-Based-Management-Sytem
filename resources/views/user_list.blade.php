5b4b1c727e1888b0d9cfab96dd9d7b7e94ba4e60
ThisWhiteYa <nantakornthidee@gmail.com>
2024-02-10 01:00:10 +0700

    Add Modal user_edit in user_list

--- resources/views/user_list.blade.php
+++ resources/views/user_list.blade.php
@@ -201,6 +201,193 @@
             </div>
         </div>
     </div>
+
+    <div class="modal fade" id="edit_user" tabindex="-1" aria-labelledby="edit_userModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="edit_userModalLabel">แก้ไขข้อมูลผู้ใช้งาน</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="w-100">
+                        <div class="row row-cols-2 p-3">
+                            <div class="mb-3 row d-flex align-items-center">
+                                <label for="plane" class="col-sm-4 col-form-label p-0 text-end">รูปโปรไฟล์</label>
+                                <div class="col-sm-8">
+                                    <input type="file" placeholder="เลือกไฟล์">
+                                </div>
+                            </div>
+                            <div class="mb-3 row">
+                                
+                            </div>
+                            <div class="mb-3 row">
+                                <label for="plane" class="col-sm-4 col-form-label p-0 pt-2 text-end">ชื่อ</label>
+                                <div class="col-sm-8">
+                                    <input type="text" class="form-control" id="plane">
+                                </div>
+                            </div>
+                            <div class="mb-3 row">
+                                <label for="plane" class="col-sm-4 col-form-label p-0 pt-2 text-end">นามสกุล</label>
+                                <div class="col-sm-8">
+                                    <input type="text" class="form-control" id="plane">
+                                </div>
+                            </div>
+                            <div class="mb-3 row">
+                                <label for="plane" class="col-sm-4 col-form-label p-0 pt-2 text-end">username</label>
+                                <div class="col-sm-8">
+                                    <input type="text" class="form-control" id="plane">
+                                </div>
+                            </div>
+                            <div class="mb-3 row">
+                                <label for="plane" class="col-sm-4 col-form-label p-0 pt-2 text-end">password</label>
+                                <div class="col-sm-8">
+                                    <input type="text" class="form-control" id="plane">
+                                </div>
+                            </div>
+                            <div class="mb-3 row">
+                                <label for="plane" class="col-sm-4 col-form-label p-0 pt-2 text-end">email</label>
+                                <div class="col-sm-8">
+                                    <input type="text" class="form-control" id="plane">
+                                </div>
+                            </div>
+                            <div class="mb-3 row">
+                                <label for="plane"
+                                    class="col-sm-4 col-form-label p-0 pt-2 text-end">ระดับการใช้งาน</label>
+                                <div class="col-sm-8">
+                                    <select class="form-select form-select-md" aria-label="Large select example">
+                                        <option value="1" selected>One</option>
+                                        <option value="2">Two</option>
+                                        <option value="3">Three</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <label for="plane" class="col-sm-4 col-form-label p-0 text-start">เลือกหน่วยงาน</label>
+                                
+                                <div id="org_grid">
+                                    <div class="col mt-2">
+                                        <input type="checkbox" class="me-2">กฟผ
+                                        <ul>
+                                            <li>
+                                                <input type="checkbox" class="me-2">กกน
+                                            </li>
+                                            <li>
+                                                <input type="checkbox" class="me-2">อบต
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <div class="col mt-2">
+                                        <input type="checkbox" class="me-2">กฟผ
+                                        <ul>
+                                            <li>
+                                                <input type="checkbox" class="me-2">กกน
+                                            </li>
+                                            <li>
+                                                <input type="checkbox" class="me-2">อบต
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <div class="col mt-2">
+                                        <input type="checkbox" class="me-2">กฟผ
+                                        <ul>
+                                            <li>
+                                                <input type="checkbox" class="me-2">กกน
+                                            </li>
+                                            <li>
+                                                <input type="checkbox" class="me-2">อบต
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <div class="col mt-2">
+                                        <input type="checkbox" class="me-2">กฟผ
+                                        <ul>
+                                            <li>
+                                                <input type="checkbox" class="me-2">กกน
+                                            </li>
+                                            <li>
+                                                <input type="checkbox" class="me-2">อบต
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <div class="col mt-2">
+                                        <input type="checkbox" class="me-2">กฟผ
+                                        <ul>
+                                            <li>
+                                                <input type="checkbox" class="me-2">กกน
+                                            </li>
+                                            <li>
+                                                <input type="checkbox" class="me-2">อบต
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <div class="col mt-2">
+                                        <input type="checkbox" class="me-2">กฟผ
+                                        <ul>
+                                            <li>
+                                                <input type="checkbox" class="me-2">กกน
+                                            </li>
+                                            <li>
+                                                <input type="checkbox" class="me-2">อบต
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <div class="col mt-2">
+                                        <input type="checkbox" class="me-2">กฟผ
+                                        <ul>
+                                            <li>
+                                                <input type="checkbox" class="me-2">กกน
+                                            </li>
+                                            <li>
+                                                <input type="checkbox" class="me-2">อบต
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <div class="col mt-2">
+                                        <input type="checkbox" class="me-2">กฟผ
+                                        <ul>
+                                            <li>
+                                                <input type="checkbox" class="me-2">กกน
+                                            </li>
+                                            <li>
+                                                <input type="checkbox" class="me-2">อบต
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <div class="col mt-2">
+                                        <input type="checkbox" class="me-2">กฟผ
+                                        <ul>
+                                            <li>
+                                                <input type="checkbox" class="me-2">กกน
+                                            </li>
+                                            <li>
+                                                <input type="checkbox" class="me-2">อบต
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <div class="col mt-2">
+                                        <input type="checkbox" class="me-2">กฟผ
+                                        <ul>
+                                            <li>
+                                                <input type="checkbox" class="me-2">กกน
+                                            </li>
+                                            <li>
+                                                <input type="checkbox" class="me-2">อบต
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save</button>
+                </div>
+            </div>
+        </div>
+    </div>
     <div class="bg-secondary p-2 text-white d-flex align-items-center w-100 justify-content-between mb-3">
         <p class="m-0 fs-5">รายชื่อผู้ใช้งาน</p>
         <a href="#" data-bs-toggle="modal" data-bs-target="#add_user">
@@ -228,7 +415,7 @@
                 <td>Hatfield</td>
                 <td>
                     <div class="icon col-3 text-end">
-                        <a href="#" class="text-decoration-none">
+                        <a href="#" class="text-decoration-none" data-bs-toggle="modal" data-bs-target="#edit_user">
                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 12 12">
                                 <path fill="#000000"
                                     d="M10.443 1.56a1.914 1.914 0 0 0-2.707 0l-.55.551a.506.506 0 0 0-.075.074l-5.46 5.461a.5.5 0 0 0-.137.255l-.504 2.5a.5.5 0 0 0 .588.59l2.504-.5a.5.5 0 0 0 .255-.137l6.086-6.086a1.914 1.914 0 0 0 0-2.707M7.502 3.21l1.293 1.293L3.757 9.54l-1.618.324l.325-1.616zm2 .586L8.209 2.502l.234-.234A.914.914 0 1 1 9.736 3.56z">
@@ -250,29 +437,7 @@
                 <td>Hatfield</td>
                 <td>
                     <div class="icon col-3 text-end">
-                        <a href="#" class="text-decoration-none">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 12 12">
-                                <path fill="#000000"
-                                    d="M10.443 1.56a1.914 1.914 0 0 0-2.707 0l-.55.551a.506.506 0 0 0-.075.074l-5.46 5.461a.5.5 0 0 0-.137.255l-.504 2.5a.5.5 0 0 0 .588.59l2.504-.5a.5.5 0 0 0 .255-.137l6.086-6.086a1.914 1.914 0 0 0 0-2.707M7.502 3.21l1.293 1.293L3.757 9.54l-1.618.324l.325-1.616zm2 .586L8.209 2.502l.234-.234A.914.914 0 1 1 9.736 3.56z">
-                                </path>
-                            </svg>
-                        </a>
-                    </div>
-                </td>
-                <td class="d-flex justify-content-center">
-                    <span>
-                        <div id="circle"
-                            style="width: 30px; height: 30px; border-radius: 100%; background-color: green;border: 1px black solid;">
-                        </div>
-                    </span>
-                </td>
-            </tr>
-            <tr>
-                <td>Howard</td>
-                <td>Hatfield</td>
-                <td>
-                    <div class="icon col-3 text-end">
-                        <a href="#" class="text-decoration-none">
+                        <a href="#" class="text-decoration-none" data-bs-toggle="modal" data-bs-target="#edit_user">
                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 12 12">
                                 <path fill="#000000"
                                     d="M10.443 1.56a1.914 1.914 0 0 0-2.707 0l-.55.551a.506.506 0 0 0-.075.074l-5.46 5.461a.5.5 0 0 0-.137.255l-.504 2.5a.5.5 0 0 0 .588.59l2.504-.5a.5.5 0 0 0 .255-.137l6.086-6.086a1.914 1.914 0 0 0 0-2.707M7.502 3.21l1.293 1.293L3.757 9.54l-1.618.324l.325-1.616zm2 .586L8.209 2.502l.234-.234A.914.914 0 1 1 9.736 3.56z">
@@ -289,6 +454,29 @@
                     </span>
                 </td>
             </tr>
+            <tr>
+                <td>Howard</td>
+                <td>Hatfield</td>
+                <td>
+                    <div class="icon col-3 text-end">
+                        <a href="#" class="text-decoration-none" data-bs-toggle="modal" data-bs-target="#edit_user">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 12 12">
+                                <path fill="#000000"
+                                    d="M10.443 1.56a1.914 1.914 0 0 0-2.707 0l-.55.551a.506.506 0 0 0-.075.074l-5.46 5.461a.5.5 0 0 0-.137.255l-.504 2.5a.5.5 0 0 0 .588.59l2.504-.5a.5.5 0 0 0 .255-.137l6.086-6.086a1.914 1.914 0 0 0 0-2.707M7.502 3.21l1.293 1.293L3.757 9.54l-1.618.324l.325-1.616zm2 .586L8.209 2.502l.234-.234A.914.914 0 1 1 9.736 3.56z">
+                                </path>
+                            </svg>
+                        </a>
+                    </div>
+                </td>
+                <td>
+                    <span class="d-flex justify-content-center">
+                        <div id="circle"
+                            style="width: 30px; height: 30px; border-radius: 100%; background-color: green;border: 1px black solid;">
+                        </div>
+                    </span>
+                </td>
+            </tr>
+         
         </tbody>
     </table>
     </div>
