26be094bd3fc394a9a8c131b6058591b10a66a85
Panuphong <panuphong.s2003@gmail.com>
2024-02-12 20:34:39 +0700

    hot fixed

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -2,21 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
     public static function getAll() {
-        $ProjectAll = [
-            [
-                "Project_name" => "โปรเจค",
-                "project_head" => "	นันทกร",
-                "butter_sourc" => "มว.",  // users
-                "balance_type" => "ลงทุน", // table balance
-                "balance" => "1150" ,
-                "org_name" => "กฟอ."
-            ],
-        ];
+
+        $ProjectAll = Project::all();
+
+        // $ProjectAll = [
+        //     [
+        //         "Project_name" => "โปรเจค",
+        //         "project_head" => "	นันทกร",
+        //         "budget_source" => "มว.",  // users
+        //         "balance_type" => "ลงทุน", // table balance
+        //         "balance" => "1150" ,
+        //         "org_name" => "กฟอ."
+        //     ],
+        // ];
 
         return view('staff/fiscal_years', compact('ProjectAll'));
     }
