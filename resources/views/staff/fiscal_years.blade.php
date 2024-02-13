4b335bf4248d037dde8f70169a4bc58f68f5de32
Surasak <92101184+Surasak0510@users.noreply.github.com>
2024-02-13 21:56:52 +0700

    Edit dynamic page fiscal_years

--- resources/views/staff/fiscal_years.blade.php
+++ resources/views/staff/fiscal_years.blade.php
@@ -374,14 +374,13 @@
                                         <div class="mb-3 row text-end">
                                             <label for="name" class="col-sm-3 col-form-label">ชื่อ</label>
                                             <div class="col-sm-9">
-                                                <input type="text" class="form-control" id="name">
+                                                <input type="text" class="form-control" id="name" >
                                             </div>
                                         </div>
                                         <div class="mb-3 row text-end">
                                             <label for="stg" class="col-sm-3 col-form-label">ยุทธศาสตร์</label>
                                             <div class="col-sm-9">
-                                                <select class="form-select" id="floatingSelect"
-                                                    aria-label="Floating label select example">
+                                                <select class="form-select" id="floatingSelect" aria-label="Floating label select example">
                                                     <option selected>Open this select menu</option>
                                                     <option value="1">One</option>
                                                     <option value="2">Two</option>
@@ -392,8 +391,7 @@
                                         <div class="mb-3 row text-end">
                                             <label for="name" class="col-sm-3 col-form-label">ผลผลิต</label>
                                             <div class="col-sm-9">
-                                                <select class="form-select" id="floatingSelect"
-                                                    aria-label="Floating label select example">
+                                                <select class="form-select" id="floatingSelect" aria-label="Floating label select example">
                                                     <option selected>Open this select menu</option>
                                                     <option value="1">One</option>
                                                     <option value="2">Two</option>
@@ -558,24 +556,25 @@
                         @foreach($ProjectAtAll as $ProjectAt)
                             <tr>
                                 <td><a href="#" class="text-black">{{ $ProjectAt->project_name }}</a></td>
-                                <td><a href="#" class="text-black">{{ $ProjectAt->project_head }}</a></td>
+                                <td><a href="#" class="text-black">{{ $ProjectAt->project_head }}</a></td>                                
                                 <td>{{ $ProjectAt->budget_source }}</td>
                                 <td>{{ $ProjectAt->budget_type }}</td>
                                 <td>{{ number_format($ProjectAt->balance) }}</td>
                                 {{-- <td>{{ $ProjectAt->org_name }}</td> --}}
                                 <td>ไม่รู้ รอคุย</td>
                                 <td>
-                                    <a href="#" data-bs-toggle="modal" data-bs-target="#edit_project">
+                                    <a href="#" data-bs-toggle="modal" data-bs-target="#edit_project" >
                                         <svg class="d-flex align-items-center" xmlns="http://www.w3.org/2000/svg" width="20"
                                             height="20" viewBox="0 0 24 24">
                                             <path fill="#000000"
                                                 d="m11.4 18.161l7.396-7.396a10.289 10.289 0 0 1-3.326-2.234a10.29 10.29 0 0 1-2.235-3.327L5.839 12.6c-.577.577-.866.866-1.114 1.184a6.556 6.556 0 0 0-.749 1.211c-.173.364-.302.752-.56 1.526l-1.362 4.083a1.06 1.06 0 0 0 1.342 1.342l4.083-1.362c.775-.258 1.162-.387 1.526-.56c.43-.205.836-.456 1.211-.749c.318-.248.607-.537 1.184-1.114m9.448-9.448a3.932 3.932 0 0 0-5.561-5.561l-.887.887l.038.111a8.754 8.754 0 0 0 2.092 3.32a8.754 8.754 0 0 0 3.431 2.13z">
                                             </path>
                                         </svg>
+                                        @php($ProjectOn = $loop->index)
                                     </a>
                                 </td>
                             </tr>
-                        @endforeach 
+                        @endforeach                                      
                     @endif
                 </tbody>
             </table>
@@ -584,6 +583,7 @@
     </div>
     </div>
     <script>
+
         var table = $('table.display').DataTable({
             pageLength: 5,
             lengthMenu: [
