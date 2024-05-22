a4a6a2d410c812e60f173e7605ca4e7655840eaf
WhiteYa <nantakornthidee@gmail.com>
2024-05-22 22:15:28 +0700

    Set layout responesive and add a little design of gauge chart

--- resources/views/executive/stg_overview.blade.php
+++ resources/views/executive/stg_overview.blade.php
@@ -12,17 +12,40 @@
             stroke: none;
             /* Remove stroke */
         }
+
+        .chart-container {
+            display: grid;
+            grid-template-columns: auto auto;
+            gap: 10px;
+        }
+
+        .chart-container .text-center {
+            box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,
+                rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
+            border-radius: 5px;
+            padding: 1rem;
+        }
+
+        @media only screen and (max-width:700px) {
+            .chart-container {
+                grid-template-columns: auto;
+            }
+        }
     </style>
 @endsection
 @section('content')
-    <div class="row">
-        <div class="col-6 text-center">
+    <div class="chart-container">
+        <div class="text-center">
             <label for="chart_div1">STG 1</label>
             <div id="chart_div1" class="d-flex justify-content-center"></div>
         </div>
-        <div class="col-6 text-center">
+        <div class="text-center">
             <label for="chart_div2">STG 2</label>
             <div id="chart_div2" class="d-flex justify-content-center"></div>
+        </div>
+        <div class="text-center">
+            <label for="chart_div3">STG 3</label>
+            <div id="chart_div3" class="d-flex justify-content-center"></div>
         </div>
     </div>
 
@@ -68,12 +91,30 @@
                 blueFrom: 100,
                 blueTo: 120,
             };
+            var data3 = google.visualization.arrayToDataTable([
+                ['Label', 'Value'],
+                ['', 70]
+            ]);
+            var options2 = {
+                width: 200,
+                height: 200,
+                redFrom: 0,
+                redTo: 70,
+                yellowFrom: 70,
+                yellowTo: 90,
+                greenFrom: 90,
+                greenTo: 100,
+                blueFrom: 100,
+                blueTo: 120,
+            };
 
 
             var chart = new google.visualization.Gauge(document.getElementById('chart_div1'));
             chart.draw(data1, options);
             var chart = new google.visualization.Gauge(document.getElementById('chart_div2'));
             chart.draw(data2, options);
+            var chart = new google.visualization.Gauge(document.getElementById('chart_div3'));
+            chart.draw(data3, options);
         }
     </script>
 @endsection
