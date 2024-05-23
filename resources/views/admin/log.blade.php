8b7de709d5c718f3b72b32fae45ac9ad22371b95
Panuphong <panuphong.s2003@gmail.com>
2024-05-23 19:55:03 +0700

    fetch data from log api and add date picker

--- resources/views/admin/log.blade.php
+++ resources/views/admin/log.blade.php
@@ -6,70 +6,106 @@
     <div class="fs-5">Log</div>
 @endsection
 @section('content')
+    <script>
+        const onDateChange = ()=>{
+            let e = document.getElementById("Date-input");
+            let date = e.value;
+
+            $.get("/api/log/"+date)
+            .done((data) =>{
+                let table = $("#log-table").dataTable().api();
+                        table.clear();
+                        //mapping data from api to <tr> object
+                        $.each(data,function(index,item){
+                            var newRow = $('<tr></tr>');
+                            newRow.append('<td>' + (index + 1) + '</td>');                            
+                            newRow.append('<td>' + item.time + '</td>');                            
+                            newRow.append('<td>' + item.by + '</td>');
+                            (item.type == "Warning") ?  
+                                newRow.append('<td style="color:orange;font-weight:bold;">' + item.type + '</td>')
+                                :
+                                newRow.append('<td style="color:green;font-weight:bold;">' + item.type + '</td>');
+
+                            newRow.append('<td>' + item.on + '</td>');
+                            newRow.append('<td>' + item.message + '</td>');
+
+                            
+                            table.row.add(newRow);
+                        });
+
+                        // redraw table
+                        table.draw();
+            });
+        }
+    </script>
+    
+
     <div>
-        <table class="table display">
+        {{-- init load data from log api --}}
+        <script>
+            $(document).ready(function() {
+                let e = document.getElementById("Date-input");
+                let date = e.value;
+
+                $.ajax({
+                    url: "/api/log/" + date, // Replace with your actual route
+                    success: function(data) {
+                        // dataTable Api as JQuery object
+                        let table = $("#log-table").dataTable().api();
+
+                        //mapping data from api to <tr> object
+                        $.each(data,function(index,item){
+                            var newRow = $('<tr></tr>');
+                            newRow.append('<td>' + (index + 1) + '</td>');                            
+                            newRow.append('<td>' + item.time + '</td>');                            
+                            newRow.append('<td>' + item.by + '</td>');
+                            (item.type == "Warning") ?  
+                                newRow.append('<td style="color:orange;font-weight:bold;">' + item.type + '</td>')
+                                :
+                                newRow.append('<td style="color:green;font-weight:bold;">' + item.type + '</td>');
+
+                            newRow.append('<td>' + item.on + '</td>');
+                            newRow.append('<td>' + item.message + '</td>');
+
+                            
+                            table.row.add(newRow);
+                        });
+
+                        // redraw table
+                        table.draw();
+                    
+                    }
+                });
+            });
+
+        </script>
+        <label for="Date-input">Date</label>
+        <input type="date" id="Date-input" name="date" class="form-control" placeholder="Date" onchange="onDateChange()" value="{{Carbon\Carbon::now()->format('Y-m-d')}}">
+        
+        <table class="table display" id="log-table">
             <thead>
                 <tr>
                     <th>
-                        FisrtName
-                    </th>
-                    <th>
-                        LastName
                     </th>
                     <th>
                         Time
                     </th>
                     <th>
-                        Action
+                        By
+                    </th>
+                    <th>
+                        Type
+                    </th>
+                    <th>
+                        On
+                    </th>
+                    <th>
+                        Message
                     </th>
                 </tr>
             </thead>
-            <tbody>
-                <tr>
-                    <td>
-                        1
-                    </td>
-                    <td>
-                        TTTT
-                    </td>
-                    <td>
-                        TTTT
-                    </td>
-                    <td>
-                        TTTT
-                    </td>
-
-                </tr>
-                <tr>
-                    <td>
-                        1
-                    </td>
-                    <td>
-                        TTTT
-                    </td>
-                    <td>
-                        TTTT
-                    </td>
-                    <td>
-                        TTTT
-                    </td>
-
-                </tr>
-                <tr>
-                    <td>
-                        TTTT
-                    </td>
-                    <td>
-                        2
-                    </td>
-                    <td>
-                        TTTT
-                    </td>
-                    <td>
-                        TTTT
-                    </td>
-
-                </tr>
+            <tbody id="log-table-body">
+                
             </tbody>
         </table>
     </div>
@@ -82,4 +118,7 @@
             ]
         })
     </script>
+
+
+    
 @endsection
